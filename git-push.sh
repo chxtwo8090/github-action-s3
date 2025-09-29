670772efccaa147d38dee11f85b4d76a81071a99
chxtwo <chxtwo@naver.com>
2025-09-29 15:21:31 +0900

    커밋

---
+++ git-push.sh
@@ -0,0 +1,15 @@
+#…or create a new repository on the command line
+#echo "# aws1q" >> README.md
+#git init
+git add README.md
+git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/chxtwo8090/aws1q.git
+git push -u origin main
+#…or push an existing repository from the command line
+#git remote add origin https://github.com/chxtwo8090/aws1q.git
+#git branch -M main
+#git push -u origin main
+
+#git
+
